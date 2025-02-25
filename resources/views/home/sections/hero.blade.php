dc08dd5edf1be67bf9b7fd70b99d7fbec6b11f58
Holo Nine <marius.paduraru@gmail.com>
2025-02-25 19:44:56 +0000

    Enhance hero section layout and add button styles.
    Revised the hero section HTML to include grid-based layouts, additional classes, and a call-to-action button. Introduced new button styles in a global CSS file and updated the hero's CSS for improved responsiveness. Adjusted global font and spacing variables for better scalability.

--- resources/views/home/sections/hero.blade.php
+++ resources/views/home/sections/hero.blade.php
@@ -1,7 +1,28 @@
-<section id="hero">
-    <h1 class="display">Crafting Great Stories with Code</h1>
-    <p>Hey, I’m Marius. I started out in electrical engineering, but along the way, I discovered my passion for building
-        interactive experiences. Now, I’m a web developer specialising in React, Laravel, WordPress, and HubSpot. I
-        believe every project tells a story, and I bring those stories to life by crafting experiences that truly
-        connect with people.</p>
+<section id="hero" class="hero">
+    <div class="col-span-4 md:col-span-5 lg:col-span-6 lg:col-start-2 xl:col-span-5 xl:col-start-2">
+        <h1 class="display mb-2.5">Crafting Great Stories with Code</h1>
+        <p>Hey, I’m Marius. I started out in electrical engineering, but along the way, I discovered my passion for
+            building interactive experiences. Now, I’m a web developer specialising in React, Laravel, WordPress, and
+            HubSpot. I believe every project tells a story, and I bring those stories to life by crafting experiences
+            that truly connect with people.</p>
+
+        <div class="button-group">
+            <a href="#" class="button">
+                <div class="button-label">See My Work</div>
+                <div class="button-arrow">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 12H6.5M20 12L14 6M20 12L14 18M20 12H9.5" stroke="currentColor" stroke-width="1.5"
+                              stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+                <div class="button-background"></div>
+            </a>
+        </div>
+
+    </div>
+    <div class="col-span-4 md:col-span-3 md:col-start-6 lg:col-span-3 lg:col-start-9 xl:col-span-3 xl:col-start-9">
+        right
+    </div>
+
 </section>
